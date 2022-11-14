0605fd7b00a7338f515b8f42acb73ca36dba2847
WariaX <115689237+WariaX@users.noreply.github.com>
2022-11-14 12:18:38 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,22 @@
+def is_perfect(n):
+    result = []    
+    for a in range(1, (int(n/2) + 1)):
+        if n % a == 0:
+           result.append(a)
+    if sum(result) == n:
+        return True
+    return False     
+
+  
+
+
 def get_perfect_numbers(n):
-    return []
+    result = []
+    i = 0
+    while len(result) != n: 
+        i = i + 1       
+        if is_perfect(i) == True:
+                result.append(i)
+
+               
+    return result 
