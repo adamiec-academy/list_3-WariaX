e5c5c492ab973b0fc9c07dee8ef802ed3020b535
WariaX <115689237+WariaX@users.noreply.github.com>
2022-11-21 13:09:31 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,1 +1,41 @@
-# Remove this comment to confirm that this task is done
+from turtle import *
+
+speed ("fast")
+
+a = 200
+b = 100
+
+def prostokat(a, b):
+    for _ in range(1):
+        forward(a)
+        left(120)
+        forward(a)
+        left(120)
+        forward(a)
+        forward(b)
+        right(120)
+        forward(b)
+        right(120)
+        forward(b)
+        left(90)
+        forward(50)
+        left(90)
+        forward(50)
+        left(90)
+        forward(50)
+        left(90)
+        forward(50)
+
+
+
+ilosc = 50
+kat = 360/ilosc
+
+for _ in range(ilosc):
+    prostokat(a, b)
+    left(kat)
+
+
+
+
+exitonclick()
